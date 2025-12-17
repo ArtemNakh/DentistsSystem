42a2c10d420c0db9351a01babddea3368ad2dfc2
ArtemNakh <artem.nakhman@gmail.com>
2025-12-17 11:37:49 +0200

    fix condif and add sequelize module

--- backend/dentistry_backend/config.ts
+++ backend/dentistry_backend/config.ts
@@ -1,41 +1,7 @@
-// //осьновний конфіг
-// const merge = require("lodash/merge")
-// if (typeof document !== "undefined")
-//   throw new Error("Don`t import config.ts from inside the cliemt-side code!!!");
-
-// const isDev = process.env.ENVIRONMENT !== "prod" ? true : false;
-
-
-// const prodConfig = {
-//   dev: isDev,
-//   environment: process.env.ENVIRONMENT,
-//   baseUrl: process.env.baseUrl,
-// };
-
-// let localConfig = {};
-
-// if (isDev) {
-//   try {
-//     localConfig = require("./config.local.ts");
-//   } catch (ex) {
-//     console.log("ex", ex);
-//     console.log("config.local does not exist");
-//   }
-// }
-
-// module.exports = merge(prodConfig, localConfig ?? {});
-
-
 
 // config.ts
 export default () => ({
   environment: process.env.ENVIRONMENT || 'dev',
   port: parseInt(process.env.PORT ?? '5000', 10),
-  database: {
-    host: process.env.MYSQL_HOST ?? 'localhost',
-    user: process.env.MYSQL_USER ?? 'root',
-    db: process.env.MYSQL_DB ?? 'DefaultNameDB',
-    password: process.env.MYSQL_PASSWORD ?? '',
-    port: parseInt(process.env.MYSQL_PORT ?? '3306', 10),
-  },
+ 
 });
--- backend/dentistry_backend/src/app.module.ts
+++ backend/dentistry_backend/src/app.module.ts
@@ -4,13 +4,36 @@ import { AppService } from './app.service';
 import { DentistryModule } from './dentistry/dentistry.module';
 import { ConfigModule } from '@nestjs/config';
 import config from '../config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [ ConfigModule.forRoot({
+  //підключає модуль конфігурації,
+  // робить його глобальним,
+  // завантажує кастомну конфігурацію з функції config.
+  imports: [
+    ConfigModule.forRoot({
       isGlobal: true,
       load: [config],
-    }),DentistryModule],
-   
+    }),
+
+    // https://docs.nestjs.com/recipes/sql-sequelize
+    // i stoped on this words(Model injection#
+    // In Sequelize the Model defines a table in the database. Instances of this class represent a database row. Firstly, we need at least one entity:))
+    SequelizeModule.forRoot({
+      dialect: process.env.DIALECT as any,
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      autoLoadModels: false,
+      synchronize: false,
+      models: [],
+    }),
+
+    DentistryModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
