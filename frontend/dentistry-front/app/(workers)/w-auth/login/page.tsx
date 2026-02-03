2354b8b81dff3b1f01b5c85c54d65e8651a25606
ArtemNakh <artem.nakhman@gmail.com>
2026-02-03 22:51:31 +0200

    added translate on worker login page

--- frontend/dentistry-front/app/(workers)/w-auth/login/page.tsx
+++ frontend/dentistry-front/app/(workers)/w-auth/login/page.tsx
@@ -7,6 +7,7 @@ import PasswordField from "./components/PasswordField";
 import SubmitButton from "./components/SubmitButton";
 import { loginWorker } from "./services/loginService";
 import { useTranslation } from "react-i18next";
+import LanguageSwitch from "@/app/components/LanguageSwitch";
 
 export default function WorkerLogin() {
   const { t } = useTranslation();
@@ -30,8 +31,15 @@ export default function WorkerLogin() {
   return (
     <>
       <label className="pb-4 flex justify-center text-2xl ">
-        {t("loginWorking.title")}
+        {t("loginWorker.title")}
       </label>
+
+      <LanguageSwitch
+        buttonClassName="-mt-5   mb-3 rounded px-3 py-1 text-white bg-soft-purple-5 hover:bg-amethyst border border-purple-900 transition duration-200"
+        dropdownClassName="absolute left-1/2 -translate-x-1/2 mt-2 w-auto rounded-md shadow-lg border border-purple-900 bg-[#7555AD] text-purple-200 z-50"
+        itemClassName="w-full px-4 py-2 text-sm hover:bg-slate-blue cursor-pointer"
+        activeItemClassName="bg-royal-purple font-bold"
+      />
 
       <Formik
         initialValues={{
--- frontend/dentistry-front/app/(workers)/w-auth/login/schemes/login.scheme.ts
+++ frontend/dentistry-front/app/(workers)/w-auth/login/schemes/login.scheme.ts
@@ -1,16 +1,17 @@
 // validationSchemas.ts
 import * as Yup from "yup";
+import i18n from "i18next";
 
 const loginValidationSchema = Yup.object().shape({
   login: Yup.string()
-    .required("error.loginWorker.loginfield.required")
-    .min(5, "error.loginWorker.loginfield.min" + " 5")
-    .max(100, "error.loginWorker.loginfield.max" + " 100"),
+    .required(() => i18n.t("error.loginWorker.loginField.required"))
+    .min(5, () => i18n.t("error.loginWorker.loginField.min"))
+    .max(100, () => i18n.t("error.loginWorker.loginField.max")),
 
   password: Yup.string()
-    .required("error.loginWorker.password.required")
-    .min(5, "error.loginWorker.password.min" + " 5")
-    .max(100, "error.loginWorker.password.max" + " 100"),
+    .required(() => i18n.t("error.loginWorker.password.required"))
+    .min(5, () => i18n.t("error.loginWorker.password.min"))
+    .max(100, () => i18n.t("error.loginWorker.password.max")),
 });
 
 export default loginValidationSchema;
