3986befadced82610193a77bf1db00822730dfb6
ArtemNakh <artem.nakhman@gmail.com>
2026-02-05 12:13:30 +0200

    fix problem redux (with unreal entity)

--- frontend/dentistry-front/lib/redux/registry.ts
+++ frontend/dentistry-front/lib/redux/registry.ts
@@ -1,7 +1,8 @@
 // registry.ts
 export const reducersRegistry: Record<string, any> = {};
 export const sagasRegistry: any[] = [];
-    
+export const entitiesRegistry: string[] = []; // список усіх сутностей
+
 export function Entity(config: { name: string }) {
   return function (target: any) {
     // створюємо екземпляр сутності
@@ -12,5 +13,8 @@ export function Entity(config: { name: string }) {
 
     // реєструємо saga‑watcher
     sagasRegistry.push(instance);
+
+    // додаємо імʼя сутності у список
+    entitiesRegistry.push(config.name);
   };
 }
--- frontend/dentistry-front/lib/redux/rootReducer.ts
+++ frontend/dentistry-front/lib/redux/rootReducer.ts
@@ -1,10 +1,24 @@
 // rootReducer.ts
 import { combineReducers } from "redux";
-import { reducersRegistry } from "./registry";
+import { entitiesRegistry, reducersRegistry } from "./registry";
 
-export const rootReducer = combineReducers(
-  // reducersRegistry
-  Object.keys(reducersRegistry).length > 0
-    ? reducersRegistry
-    : { _empty: (state = {}) => state },
+// export const rootReducer = combineReducers(
+//   // reducersRegistry
+//   Object.keys(reducersRegistry).length > 0
+//     ? reducersRegistry
+//     : { _empty: (state = {}) => state },
+// );
+
+// створюємо дефолтні редʼюсери для всіх сутностей
+
+const defaultReducers = entitiesRegistry.reduce(
+  (acc, name) => {
+    acc[name] = (state = {}) => state;
+    return acc;
+  },
+  {} as Record<string, any>,
 );
+export const rootReducer = combineReducers({
+  ...defaultReducers,
+  ...reducersRegistry,
+});
